249f6add18953a3d6ceb0248f2d25e9530a27a9f
carlleton <carlleton@hotmail.com>
2018-01-21 23:39:38 +0800

    实现图片copy、图片下载、识别tag标签、替换七牛域名

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+var http = require("http")
 var fs = require('fs')
 var mysql    = require('mysql')
 var moment   = require('moment')
 const bluebird = require('bluebird')
 const Promise = bluebird.Promise
-var Markdown = require('./tools/Markdown')
+var Markdown = require('./tools/to-markdown')
 
-var outfolder = './out' // 到处md目录
+var outfolder = './out' // md输出目录
+var imgfromfolder = 'E:\\www\\emlog' //图片copy来源目录
+var imgoutfolder = './outimg' // 图片输出目录
+var replacehost = 'http://www.example.com' // 要处理的域名
+var qiniuhost = 'http://xx.xx.qiniucloud.com' //要替换的七牛域名
+
 
 var asynclength = 5
 var users
@@ -21,7 +27,7 @@ var connection = mysql.createConnection({
 })
 
 connection.connect()
-
+// 查询数据库
 function querydb (sql) {
   return new Promise(function (resolve, reject) {
     connection.query(sql, function (error, results, fields) {
@@ -34,20 +40,150 @@ function querydb (sql) {
     })
   })
 }
+// 识别图片
+function readimg (content, gid) {
+  const pattern = /!\[(.*?)\]\((.*?)\)/mg
+  const result = []
+  let matcher
+  while ((matcher = pattern.exec(content)) !== null) {
+    var src = matcher[2]
+    if (src.indexOf(' ') > -1) {
+      src = src.substr(0, src.indexOf(' '))
+    }
+    if (src == '') continue
+    result.push({
+      dst: matcher[0],
+      alt: matcher[1],
+      url: src
+    })
+  }
+  return new Promise(async function (resolve, reject) {
+    if (result.length == 0) {
+      resolve(content)
+      return
+    }
+    for (let i = 0, n = result.length; i < n; i++) {
+      var url = result[i].url
+      var oldurl = url
+      url = url.replace(replacehost, '')
+      if (url.indexOf('/path/to/') == 0) continue
+      if (url.indexOf('data:image') == 0) continue
+      if (url.indexOf('/admin/editor/plugins/emoticons/') > -1) {
+        content = content.replace(result[i].dst, '')
+        continue
+      }
+      if (url.indexOf('http://') == 0 || url.indexOf('https://') == 0) {
+        var filename = url.substr(url.lastIndexOf('/'))
+        var ext = filename.substr(filename.indexOf('.'))
+        if (['.png','.jpeg','.jpg','.gif'].indexOf(ext) == -1) {
+          ext = '.jpg'
+        }
+        filename = filename.substr(0, filename.indexOf('.')) + ext
+        var dst = '/201801/' + filename
+        var newfilename = '/UserFiles/image' + dst
+        dst = imgoutfolder + dst
+        var dstfolder = dst.substr(0, dst.lastIndexOf('/'))
+        if (!fs.existsSync(dstfolder)) {
+          fs.mkdirSync(dstfolder);
+        }
+        await http.get(url, async function(res){
+          var imgData = ""
+          res.setEncoding("binary") //一定要设置response的编码为binary否则会下载下来的图片打不开
+          res.on("data", function (chunk) {
+            imgData += chunk
+          })
+          res.on("end", async function () {
+            if (!fs.existsSync(dstfolder)) {
+              fs.mkdirSync(dstfolder);
+            }
+            await fs.writeFile(dst, imgData, "binary", function (err) {
+              if (err) {
+                console.log("下载失败：" + url)
+              } else {
+                newfilename = newfilename.replace('/UserFiles/image', qiniuhost)
+                content = content.replace(url, newfilename)
+                // console.log("down success");
+              }
+            })
+          })
+        })
+      } else {
+        url = url.replace('/content/uploadfile', '/UserFiles/image')
+        url = url.replace('/uploadfile', '/UserFiles/image')
+        url = url.replace('/Files/img', '/UserFiles/image')
+        if (url.indexOf('/images') == 0) {
+          url = '/UserFiles/image' + url.replace('/images', '')
+        }
+        var src = imgfromfolder + url
+        var dst = imgoutfolder + url.replace('/UserFiles/image', '').replace('/UserFiles', '').replace('!600', '')
+        var filename = url.substr(url.lastIndexOf('/'))
+        var fileoldname = filename
+        var ext = filename.substr(filename.indexOf('.'))
+        if (['.png','.jpeg','.jpg','.gif'].indexOf(ext) == -1) {
+          ext = '.jpg'
+        }
+        filename = filename.substr(0, filename.indexOf('.')) + ext
+        dst = dst.replace(fileoldname, filename)
+        url = url.replace(fileoldname, filename)
+        var dstfolder = dst.substr(0, dst.lastIndexOf('/'))
+
+        if (!fs.existsSync(dstfolder)) {
+          fs.mkdirSync(dstfolder);
+        }
+        if (fs.existsSync(src)) {
+          await fs.writeFileSync(dst, fs.readFileSync(src))
+          url = url.replace('/UserFiles/image', qiniuhost)
+          content = content.replace(oldurl, url)
+          // await fs.createReadStream(src).pipe(fs.createWriteStream(dst))
+        } else {
+          console.log('不存在：' + src + ',gid:' + gid)
+        }
+      }
+    }
+    resolve(content)
+  })
+}
+// 处理tag链接，主要是处理tag中相应的域名
+function readtag (content) {
+  var arr = content.split('[')
+  if (arr.length <= 1) {
+    return content
+  }
+  for (var i = 1, n = arr.length; i < n; i++) {
+    var str = '[' + arr[i]
+    const pattern = /\[([^(\])]*)\]\((http:\/\/(www\.)?example.com)?\/tag\/[^(\))]*\)/g
+    const result = []
+    let matcher
+    while ((matcher = pattern.exec(content)) !== null) {
+      var src = matcher[2]
+      result.push({
+        dst: matcher[0],
+        alt: matcher[1],
+        url: src
+      })
+    }
+    for (var j = 0, m = result.length; j < m; j++) {
+      // console.log(result[j].dst,result[j].alt)
+      content = content.replace(result[j].dst,result[j].alt)
+    }
+  }
+  return content
+}
 
 async function readblog (isreset) {
   if (!isreset) {
     blogi++
     if (blogi > bloglist.length - 1) {
-      connection.end();
+      // connection.end();
+      console.log('content end')
       return
     }
   }
-  console.log('开始：' + blogi)
   if (!users) {
     users = await querydb('select * from emlog_user')
   }
   var blog = bloglist[blogi]
+  // console.log('开始：' + blog.gid)
   var sql = "select * from emlog_tag where gid like '%," + blog.gid + ",%'"
   tagresults = await querydb(sql)
   var tags = tagresults.map((obj) => obj.tagname).join(' ')
@@ -66,11 +202,16 @@ async function readblog (isreset) {
   var summary = blog.excerpt || ''
   if (summary) {
     summary = Markdown.toMarkdown(summary)
+    summary = summary.replace(/\n\n/g, '\n')
   }
   var content = blog.content || ''
   if (content) {
     content = Markdown.toMarkdown(content)
   }
+  // 分析图片
+  summary = await readimg(summary, gid)
+  content = await readimg(content, gid)
+  content = readtag(content)
 
   // 写入文件
   var data = `<!--
@@ -97,7 +238,8 @@ ${content}
   })
 }
 (async function () {
-  var sql = 'select emlog_blog.*,emlog_sort.sortname from emlog_blog,emlog_sort where emlog_blog.sortid=emlog_sort.sid'
+  var sql = 'select emlog_blog.*,emlog_sort.sortname from emlog_blog,emlog_sort where emlog_blog.sortid=emlog_sort.sid order by emlog_blog.gid desc'
+  // sql = 'select emlog_blog.*,emlog_sort.sortname from emlog_blog,emlog_sort where emlog_blog.sortid=emlog_sort.sid and gid=1611 order by emlog_blog.gid desc'
   bloglist = await querydb(sql)
   readblog()
 })()
